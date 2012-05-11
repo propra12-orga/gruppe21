80e2d6f03ffe3c851957958aa5d74538b8c5063e
Saber104 <Kenjiro@K7XNB.(none)>
2012-05-11 21:16:45 +0200

    modified position calculation for button positioning

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -7,37 +7,71 @@ import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
 public class MainMenu extends GraphicalGameUnit {
-	// using GameConstants for exact Buttonplacing on screen
-	private int ButtonX = GameConstants.FRAME_SIZE_X / 2 - 100;
-	private int ButtonY = (GameConstants.FRAME_SIZE_Y / 3) * 2;
-	// sets the space between all buttons
-	private int ButtonSpace = 20;
-	/*
-	 * picturetitles as strings for loading purposes not final, loading of all
-	 * pictures will be handled in Imageloader class
-	 */
-	private String menuBGImage = "/MMBGPlaceholder.png";
-	private String singleplayerActive = "/ActiveSingleplayerPlaceholder.png";
-	private String multiplayerActive = "/ActiveMultiplayerPlaceholder.png";
-	private String quitActive = "/ActiveQuitgamePlaceholder.png";
-	private String ccontinueActive = "/ActiveContinueGamePlaceholder.png";
-	private String singleplayerInactive = "/InactiveSingleplayerPlaceholder.png";
-	private String multiplayerInactive = "/InactiveMultiplayerPlaceholder.png";
-	private String quitInactive = "/InactiveQuitgamePlaceholder.png";
-	private String ccontinueInactive = "/InactiveContinueGamePlaceholder.png";
-	private String selector = "/SelectorPlaceholder.png";
-	private ImageIcon iiOne = new ImageIcon(menuBGImage);
-	private Image Background = iiOne.getImage();
 
-	// loads all images needed
+	// using GameConstants for exact Buttonplacing on screen
+	private int startXPos = GameConstants.FRAME_SIZE_X / 2 - 100;
+	private int startYPos = (GameConstants.FRAME_SIZE_Y / 3) * 2;
+
+	// sets the space between all buttons and thier positions
+	private int ButtonSpace = 20;
+	private int ButtonHeight = 50;
+	private int selectorMoveStep = ButtonSpace + ButtonHeight;
+	private int button1YPos = startYPos + selectorMoveStep;
+	private int button2YPos = startYPos + 2 * selectorMoveStep;
+	private int button3YPos = startYPos + 3 * selectorMoveStep;
+	private int button4YPos = startYPos + 4 * selectorMoveStep;
+
+	// not final, loading of all pictures will be handled in Imageloader class
+	private Image Background = new ImageIcon("/MMBGPlaceholder.png").getImage();
+	private Image select = new ImageIcon("/SelectorPlaceholder.png").getImage();
+	private Image singlePlayerActive = new ImageIcon(
+			"/ActiveSingleplayerPlaceholder.png").getImage();
+	private Image multiplayerActive = new ImageIcon(
+			"/ActiveMultiplayerPlaceholder.png").getImage();
+	private Image quitActive = new ImageIcon("/ActiveQuitgamePlaceholder.png")
+			.getImage();
+	private Image ccontinueActive = new ImageIcon(
+			"/ActiveContinueGamePlaceholder.png").getImage();
+
+	private Image singlePlayerInactive = new ImageIcon(
+			"/InactiveSingleplayerPlaceholder.png").getImage();
+	private Image multiplayerInactive = new ImageIcon(
+			"/InactiveMultiplayerPlaceholder.png").getImage();
+	private Image quitInactive = new ImageIcon(
+			"/InactiveQuitgamePlaceholder.png").getImage();
+	private Image ccontinueInactive = new ImageIcon(
+			"/InactiveContinueGamePlaceholder.png").getImage();
 
 	@Override
 	public void drawComponent(Graphics g) {
 		g.drawImage(Background, 0, 0, null);
+		g.drawImage(singlePlayerInactive, startXPos, button1YPos, null);
+		g.drawImage(multiplayerInactive, startXPos, button2YPos + ButtonSpace
+				+ ButtonHeight, null);
+		g.drawImage(quitInactive, startXPos, button3YPos + ButtonSpace
+				+ ButtonHeight, null);
+		g.drawImage(ccontinueInactive, startXPos, button4YPos + ButtonSpace
+				+ ButtonHeight, null);
+		g.drawImage(select, startXPos - (select.getWidth(null) + ButtonSpace),
+				button1YPos, null);
+
 	}
 
+	public int getX() {
+		return 0;
+	}
+
+	// MainMenu Navigation
 	@Override
 	public void handleKeyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_UP || key == KeyEvent.VK_LEFT) {
+
+		}
+		if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_RIGHT) {
+
+		}
 
 	}
 
