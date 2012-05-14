bffe6d2feaa8cfad7dcb9ec67e7991b71e5018f3
Manuel Stommel <Manuel.Stommel@hhu.de>
2012-05-14 11:39:35 +0200

    changed private attributes to protected ones

--- src/mapobjects/MoveableObject.java
+++ src/mapobjects/MoveableObject.java
@@ -3,9 +3,9 @@ package mapobjects;
 import java.awt.Graphics2D;
 
 public abstract class MoveableObject extends MapObject{
-	private Direction direction;
-	private int dx;
-	private int dy;
+	protected Direction direction;
+	protected int dx;
+	protected int dy;
 	
 	public MoveableObject(int x,int y,boolean v,boolean d,boolean c,String p){
 		super(x,y,v,d,c,p);
