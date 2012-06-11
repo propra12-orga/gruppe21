e6a193a60114ee5eeb0d2730f52e1d107a9e4442
Saber104 <Kenjiro@K7XNB.(none)>
2012-06-11 13:45:57 +0200

    Classes for multiplayer menues added.

---
+++ src/multiplayer/OptionMenu.java
@@ -0,0 +1,49 @@
+package multiplayer;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import main.GraphicalGameUnit;
+
+/**
+ * The OptionMenu class gives the user the oportunity to choose to play local
+ * multiplayer or network-based multiplayer. It is extanded from the
+ * GraphicalGameUnit class.
+ * 
+ * @author saber104
+ * 
+ */
+
+public class OptionMenu extends GraphicalGameUnit {
+
+	@Override
+	public void updateComponent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleKeyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleKeyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initComponent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawComponent(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
